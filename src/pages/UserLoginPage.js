f5b59eb9135b15e8182d7d3d05c3e702c17a0fc4
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-09 15:28:15 +0300

    pending api call

--- src/pages/UserLoginPage.js
+++ src/pages/UserLoginPage.js
@@ -1,32 +1,73 @@
 import React from "react";
 import Input from "../components/Input"
-import { login } from '../api/apiCalls'
+import { login } from "../api/apiCalls"
+import axios from "axios";
 
 class UserLoginPage extends React.Component {
     state = {
         email: null,
-        password: null
+        password: null,
+        pendingApiCall: false,
+        error: null
+    }
+
+    componentDidMount() {
+        axios.interceptors.request.use((request) => {
+            this.setState({
+                pendingApiCall: true
+            })
+
+            return request
+        })
+
+        axios.interceptors.response.use((res) => {
+            this.setState({
+                pendingApiCall: false
+            })
+
+            return res
+        }, (err) => {
+            this.setState({
+                pendingApiCall: false
+            })
+
+            throw err
+        })
     }
 
     onChange = e => {
         const { name, value } = e.target
 
         this.setState({
-            [name]: value
+            [name]: value,
+            error: null
         })
     }
 
-    onClickLogin = e => {
+    onClickLogin = async e => {
         e.preventDefault()
         const { email, password } = this.state
         const creds = {
             username: email,
             password: password
         }
-        login(creds)
+        this.setState({
+            error: null
+        })
+        try {
+            await login(creds)
+        } catch (err) {
+            this.setState({
+                error: "Incorrect email or password. Try again."
+
+            })
+        }
+
     }
 
     render() {
+        const { email, password, pendingApiCall } = this.state
+
         return (
             <div className="container">
                 <div className="row">
@@ -35,19 +76,18 @@ class UserLoginPage extends React.Component {
                             <div className="card-body p-4 p-sm-5">
                                 <h2 className="card-title text-center mb-5 fw-light">Login</h2>
                                 <form>
-                                    <Input type="email" name="email" label="Email" placeHolder="asdas@gmail.com" onChange={this.onChange} />
-                                    <Input type="password" name="password" label="Password" placeHolder="password" onChange={this.onChange} />
+                                    <Input type={email} name={email} label="Email" placeHolder="asdas@gmail.com" onChange={this.onChange} />
+                                    <Input type={password} name={password} label="Password" placeHolder="password" onChange={this.onChange} />
                                     <div className="d-grid mt-5">
-                                        <button className="btn btn-login text-uppercase fw-bold" type="submit" onClick={this.onClickLogin}>
-                                            {/* {pendingApiCall ? <div className="spinner-grow mt-2">
+                                        <button className="btn btn-login text-uppercase fw-bold" type="submit" onClick={this.onClickLogin} disabled={pendingApiCall}>
+                                            {pendingApiCall ? <div className="spinner-grow mt-2">
                                                 <span className="sr-only"></span>
-                                            </div> : "Login"} */}
-                                            Login
+                                            </div> : "Login"}
                                         </button>
                                     </div>
-                                    <div className="d-grid text-center mt-4">
-                                        <a href="#">Forgot your password or email ?</a>
-                                    </div>
+                                    {this.state.error && <div className="d-grid text-center mt-4">
+                                        <a className="loginError">{this.state.error}</a>
+                                    </div>}
                                 </form>
                             </div>
                         </div>
