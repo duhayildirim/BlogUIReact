e50e815410e37023764ccb9eec424aa3e0d82da3
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-10 13:43:42 +0300

    navbar

---
+++ src/components/TopBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class TopBar extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-9 col-md-9 col-lg-9 mx-auto">
+                        <div className="card border-0 shadow rounded-3 my-5">
+                            <div className="card-body ">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TopBar
