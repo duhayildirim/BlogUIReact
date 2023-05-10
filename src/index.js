e990098152bcbedf0a71554d3be7f5f4d05af0b1
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2023-05-10 12:46:26 +0300

    Home page created

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './styles/Form.css';
 import reportWebVitals from './reportWebVitals';
 import UserSignupPage from './pages/UserSignupPage';
 import UserLoginPage from './pages/UserLoginPage';
+import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -12,11 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <UserSignupPage /> */}
-    <UserLoginPage />
+    {/* <UserLoginPage /> */}
+    <HomePage />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12 col-md-12 col-lg-12 mx-auto">
+                    <div className="card border-0 shadow rounded-3 my-5">
+                        <div className="card-body p-4 p-sm-5">
+                            <h2 className="card-title text-center mb-5 fw-light">Home Page</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default HomePage
